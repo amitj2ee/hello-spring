cf2976d95ff402484d199f15add8ea764576d610
Amit Kaushal <amitbhabhis@gmail.com>
2026-06-20 23:46:50 +0530

    Initial Spring Boot Hello World application

---
+++ src/main/java/com/example/hello_spring/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.hello_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World from Spring Boot!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "This is a test endpoint and yes!";
+    }
+    
+}
